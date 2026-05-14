cd3565f6eb7f83546b4b18494c909126c5d43415
durdimatovmansur6-coder <durdimatovmansur6@gmail.com>
2026-05-14 22:54:24 +0500

    chore: Update Supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Netlify'da .env fayllari yuklanmagani uchun oq ekran bo'lib qolmasligi maqsadida to'g'ridan-to'g'ri ulaymiz.
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://koxyhupsszszwbbqgaag.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_U3Af28U3yjv6r28qUIghkw_0kGSuHoC'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://qqqbbdavxilljtnuaiqj.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_Fwn9yw8AcukZn-XqzjqzOQ_JgBbZ0sv'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
